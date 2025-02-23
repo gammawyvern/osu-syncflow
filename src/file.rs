9dc2c769efc3239a8d0fc951e76608b0c2d0b15d
Keagen <keegenjt@gmail.com>
2025-02-23 12:07:57 -0500

    Move in tests

--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,33 @@
+use std::fs;
 
-pub fn read_osu_file() {
+fn read_raw_osu_file(path: &str) -> Result<String, std::io::Error> {
+    fs::read_to_string(path)
+}
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_osu_file_success() {
+        let content = read_osu_file("assets/reanimate/reanimate.osu").expect("Failed to read file.");
+        assert!(content.contains("[HitObjects]"));
+    }
+
+    #[test]
+    fn test_read_osu_file_failure() {
+        let not_found = read_osu_file("assets/dne.osu");
+        assert!(not_found.is_err());
+    }
+
+    #[test]
+    fn test_read_osu_file_empty_path() {
+        let not_found = read_osu_file("");
+        assert!(not_found.is_err());
+    }
+
+    // #[test]
+    // fn test_read_osu_file_no_permissions() {
 }
 
