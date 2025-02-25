445bf4168666f67fb305328933ea37044ddc65a8
Keagen <keegenjt@gmail.com>
2025-02-25 15:10:20 -0500

    Implement first failing test

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,13 @@
 use std::collections::HashMap;
 
-fn parse_raw_osu_file(raw_file: &String) {
+fn parse_raw_osu_file(raw_file: &str) -> HashMap<String, String> {
+    let mut osu_beatmap_data = HashMap::new();
 
+    for line in raw_file.lines() {
+        println!("{}", line);
+    }
+
+    return osu_beatmap_data;
 }
 
 
@@ -10,7 +16,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_{
+    fn test_parse_raw_osu_file_has_general() {
+        // let raw_file_with_general = String::from("[General]\nSome: data");
+        let raw_file_with_general = "[General]\nSome: data";
+        let parsed_osu_file_with_general = parse_raw_osu_file(raw_file_with_general);
 
+        assert!(parsed_osu_file_with_general.contains_key("General"));
     }
 }
