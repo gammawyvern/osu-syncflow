ca1128b5bcf2a4c24e6ff5a145263f712906e4ff
Keagen <keegenjt@gmail.com>
2025-02-25 21:39:11 -0500

    Create vectors for data (just Strings right now)

--- src/parser.rs
+++ src/parser.rs
@@ -2,11 +2,15 @@ use std::collections::HashMap;
 
 pub fn parse_raw_osu_data(raw_file: &str) -> HashMap<String, Vec<String>> {
     let mut osu_beatmap_data: HashMap<String, Vec<String>> = HashMap::new();
+    let mut current_section: Option<String> = None;
 
     for line in raw_file.lines() {
         if line.starts_with('[') && line.ends_with(']') {
-            let section_name = line.trim_start_matches('[').trim_end_matches(']').to_string();
-            osu_beatmap_data.entry(section_name).or_insert_with(Vec::new);
+            let new_section = line.trim_matches(&['[', ']']);
+            current_section = Some(new_section.to_string());
+        } else if let Some(ref header) = current_section {
+            let data = line.to_string();
+            osu_beatmap_data.entry(header.clone()).or_insert_with(Vec::new).push(data);
         }
     }
 
