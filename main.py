fd840c7f0ef8d98aae15e189dbfd1dfa5dfbbd27
Keagen Talsma <keegenjt@gmail.com>
2025-08-05 20:17:55 -0400

    Add encoding because ????

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from models import TimingPoint
 from utils import file_parser
 
 def main():
-    with open("./assets/reanimate.osu", 'r') as file:
-        file_content: str = file.read() 
+    with open("./assets/reanimate.osu", 'r', encoding="utf-8") as file:
+        file_content: str = file.read()
 
     sections: Dict[str, list] = file_parser.get_sections(file_content)
 
     # Timing Point Setup
 
     timing_point_lines: List[str] = sections.get("TimingPoints")
-    timing_points: List[TimingPoint] = [TimingPoint.from_line(line) for line in timing_point_lines] 
+    timing_points: List[TimingPoint] = [TimingPoint.from_line(line) for line in timing_point_lines]
 
     # Hit Object Setup
 
